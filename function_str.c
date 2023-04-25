545abc0b531b3320cd3d252ad7982d6ea36070ef
nairbrh <houcinenair@gmail.com>
2023-04-25 01:51:46 -0700

    search code

---
+++ function_str.c
@@ -0,0 +1,107 @@
+#include "shell.h"
+/**
+ * custom_strdup - Copies a string into a newly allocated memory block
+ * @source: Pointer to the source string to be copied
+ *
+ * Return: Pointer to the newly allocated memory
+ */
+char *custom_strdup(const char *source)
+{
+	int length = custom_strlen((char *)source) + 1;
+	/* +1 for the null-terminator */
+
+	char *desstination = malloc(length * sizeof(char));
+
+	if (destination == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	custom_strcpy(destination, source);
+
+	return (destination);
+}
+/**
+*
+* custom_strcpy - Copies a string from source to destination
+* @destination: Pointer to the destination buffer
+* @source: Pointer to the source string
+* This function copies the string pointed to by source (including the
+* terminating null character) to the buffer pointed to by destination.
+* Return: Pointer to the destination buffer
+*/
+int custom_strlen(char *str)
+{
+	int length = 0;
+
+	while (str[length] != END_STRING)
+	{
+		length++;
+	}
+	return (length);
+}
+/**
+ * custom_strcmp - Compares two strings
+ * @str1: Pointer to the first string to be compared
+ * @str2: Pointer to the second string to be compared
+ *
+ * This function takes two pointers to strings as input and compares them
+ * Return: Integer value representing the result of the comparison
+ */
+int custom_strcmp(const char *str1, const char *str2)
+{
+	while (*str1 && *str2)
+	{
+		if (*str1 != *str2)
+		{
+		return (*str2 - *str2);
+		}
+		str1++;
+		str2++;
+	}
+	return (*str1 - *str2);
+}
+/**
+* Copies a string from source to destination.
+* 
+* @param destination The destination buffer.
+* 
+* @param source The source string.
+* 
+* @return The pointer to the destination buffer.
+*/
+
+void custom_strcpy(char *destination, const char *source)
+
+{
+	int i = 0;
+
+	while (source[i] != END_STRING)
+	{
+		destination[i] = source[i];
+		i++;
+	}
+	destination[i] = source[i];
+}
+
+/**
+ * custom_strcat: concatenates a source string to the end of a destination
+ *
+ * destination: a pointer of type char that represents the destination string
+ * source: a constant pointer of type char that represents the source string
+ */
+
+void custom_strcat(char *destination, const char *source)
+{
+	/* Get the length of the destination string */
+	int dest_length = custom_strlen(destination);
+	int i = 0;
+	/* Append characters from source to destination */
+	while (source[i] != END_STRING)
+	{
+		destination[dest_length + i] = source[i];
+		i++;
+	}
+	/* Append the terminating null character to the destination string */
+	destination[dest_length + i] = END_STRING;
+}
--- shell.h
+++ shell.h
@@ -1,30 +1,63 @@
 #ifndef _SHELL_H
 #define _SHELL_H
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <ctype.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+#define MAX_ENV_VARS 128
 #define PROMPT "$holbiies "
 #define BUFFER_SIZE 1024
 #define COMMAND_SIZE 64
-#define END_STRING_CHAR '\0'
-extern char *env_var[];
-extern char **env_set;
-/* extern char **environ; systeme env */
-char **parse_args(char *command);
-int custom_strlen(char *str);
-int read_line(char *buffer, int buffer_size);
+#define END_STRING '\0'
+
+extern char **environ;
+
+typedef struct command_node
+{
+	char *command;
+	struct command_node *next;
+} command_node_t;
+
+command_node_t
+
+*create_command_node(char *command);
+
+typedef struct env_var_node
+{
+	char *key;
+	char *value;
+	struct env_var_node *next;
+} env_var_node_t;
+
+env_var_node_t *env_vars[MAX_ENV_VARS] = {NULL};
+
+
+void add_command_node(command_node_t **head, char *command);
+
+
+int read_line(char **buffer, size_t *buffer_size);
+int make_directory(const char *pathname);
+int custom_setenv(const char *key, const char *value, int overwrite);
+char *custom_getenv(const char *key);
+int execute_linked_command(command_node_t *head, char *command);
 int execute_command(char *command);
+void free_command_list(command_node_t *head);
+char *search_command_in_path(const char *command);
+char **parse_args(char *command);
+
+
+int custom_strlen(char *str);
+char *custom_strdup(const char *source);
 void custom_strcpy(char *destination, const char *source);
 void custom_strcat(char *destination, const char *source);
 int file_exists(const char *filepath);
 int custom_isspace(int ch);
 int custom_strcmp(const char *str1, const char *str2);
 int custom_isprint(int ch);
+void print_help();
 
 #endif /* _SHELL_H */
